068c0b618a165d353d5dd10214cafb8a6a0643cf
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-08-21 15:39:41 -0300

    add first exercise of recipe 13

--- exercises/thirdUnit/recipe13/pages/movies.js
+++ exercises/thirdUnit/recipe13/pages/movies.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from '../style/Movies.module.css';
-
+import Link from 'next/link';
 export default function Movies({ data }) {
     const [searchQuery, setSearchQuery] = useState(""); // State to hold the search query
     const [moviesData, setMoviesData] = useState(data); // State to hold the fetched movies data
@@ -30,7 +30,9 @@ export default function Movies({ data }) {
             <div className={styles.moviesGrid}>
                 {moviesData?.Search.map((m) => (
                     <div key={m.imdbID} className={styles.movieItem}>
-                        <img src={m.Poster} alt={`${m.Title} Poster`} />
+                        <Link href={`/details?id=${m.imdbID}`}>
+                            <img src={m.Poster} alt={`${m.Title} Poster`} />
+                        </Link>
                         <div className={styles.title}>
                             {m.Title} --- {m.Year}
                         </div>
@@ -41,21 +43,21 @@ export default function Movies({ data }) {
     );
 }
 
-export async function getServerSideProps(context) {
-    const res = await fetch(`http://www.omdbapi.com/?apikey=6e8073bf&s=sunshine`);
-    const data = await res.json();
+// export async function getServerSideProps(context) {
+//     const res = await fetch(`http://www.omdbapi.com/?apikey=6e8073bf&s=sunshine`);
+//     const data = await res.json();
 
-    // if (data.Response === "False") {
-    //     return {
-    //         props: {
-    //             data: null
-    //         }
-    //     };
-    // }
+//     // if (data.Response === "False") {
+//     //     return {
+//     //         props: {
+//     //             data: null
+//     //         }
+//     //     };
+//     // }
 
-    return {
-        props: {
-            data,
-        },
-    };
-}
+//     return {
+//         props: {
+//             data,
+//         },
+//     };
+// }
